5adfa9201e6f57d02e68ba5d7646df51924f7e47
sKillseries <skillseries.infosec@gmail.com>
2025-09-07 13:44:10 +0200

    modif url to docker container name

--- src/Favorites.js
+++ src/Favorites.js
@@ -5,7 +5,7 @@ export default function Favorites() {
   const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
-    fetch("http://rss-backend.test/api/favorites")
+    fetch("http://backend:9000/api/favorites")
       .then((res) => res.json())
       .then((data) => setFavorites(data));
   }, []);
